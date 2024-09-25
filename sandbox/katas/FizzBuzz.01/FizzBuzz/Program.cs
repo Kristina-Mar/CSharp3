0117ea71b463ec02eea63a4f845bfae0f1180a8a
Kristina M <kris.martiskova@gmail.com>
2024-09-25 20:36:47 +0200

    FizzBuzz - added separate lines with explanations

--- sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 FizzBuzz fizzBuzz = new FizzBuzz();
+
+Console.WriteLine("Divisible by 3 and/or 5:");
 fizzBuzz.CountTo(100);
+
+Console.WriteLine();
+
+Console.WriteLine("Containing the numbers 3 and/or 5:");
 fizzBuzz.CountToExtra(100);
