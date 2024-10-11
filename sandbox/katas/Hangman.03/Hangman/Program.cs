5b41040b660c23b6e233c504ff6bc581d75102ab
Kristina M <kris.martiskova@gmail.com>
2024-10-11 18:52:49 +0200

    Hangman UI update

--- sandbox/katas/Hangman.03/Hangman/Program.cs
+++ sandbox/katas/Hangman.03/Hangman/Program.cs
@@ -2,6 +2,7 @@
 
 Hangman hangman = new();
 Console.WriteLine($"Welcome to the game of hangman. Guess an animal. You have a maximum of {hangman.MaxNumberOfIncorrectGuesses} incorrect guesses.");
+Console.WriteLine($"Your word: {string.Join(" ", hangman.UncoveredWord)}");
 while (hangman.GameIsInProgress)
 {
     char guessedLetter = char.ToUpper(Console.ReadKey().KeyChar);
