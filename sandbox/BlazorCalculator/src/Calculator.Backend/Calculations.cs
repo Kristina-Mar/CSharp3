6e41b7cab7cad21f973530ea7100afcf1f799478
Kristina M <kris.martiskova@gmail.com>
2024-11-15 18:13:21 +0100

    Optional assignment for lesson 8 - Blazor UI for a calculator. It is an adaptation of my final project for my C# 1 Czechitas course with WinForms UI.

---
+++ sandbox/BlazorCalculator/src/Calculator.Backend/Calculations.cs
@@ -0,0 +1,32 @@
+﻿namespace Calculator.Backend;
+
+public class Calculations
+{
+    public double Result { get; set; }
+
+    public void SetFirstNumber(double number1) => Result = number1;
+
+    public double Calculate(string symbol, double number2)
+    {
+        switch (symbol)
+        {
+            case "+":
+                Result += number2;
+                return Result;
+            case "-":
+                Result -= number2;
+                return Result;
+            case "*":
+                Result *= number2;
+                return Result;
+            case "÷":
+                Result /= number2;
+                return Result;
+            case "^":
+                Result = Math.Pow(Result, number2);
+                return Result;
+            default: // This case won't happen.
+                return 0;
+        }
+    }
+}
