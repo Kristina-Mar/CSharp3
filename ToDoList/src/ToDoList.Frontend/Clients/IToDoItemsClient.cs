e508fa0b07258377b819e5eeee4f84331ee106d5
Kristina M <kris.martiskova@gmail.com>
2024-11-21 10:51:58 +0100

    HTTP Client - added remaining CRUD operations

--- ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -5,4 +5,8 @@ using ToDoList.Frontend.Views;
 public interface IToDoItemsClient
 {
     public Task<List<ToDoItemView>> ReadItemsAsync();
+    public Task<ToDoItemView?> ReadByIdAsync(int itemId);
+    public Task CreateAsync(ToDoItemView toDoItemView);
+    public Task UpdateByIdAsync(ToDoItemView toDoItemView);
+    public Task DeleteByIdAsync(ToDoItemView toDoItemView);
 }
--- ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -4,6 +4,30 @@ using ToDoList.Frontend.Clients;
 
 public class ToDoItemsClient(HttpClient httpClient) : IToDoItemsClient
 {
+    public async Task CreateAsync(ToDoItemView toDoItemView)
+    {
+        var itemToCreateDto = new ToDoItemCreateRequestDto(toDoItemView.Name, toDoItemView.Description, toDoItemView.IsCompleted);
+        try
+        {
+            var response = await httpClient.PostAsJsonAsync("api/ToDoItems", itemToCreateDto);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                Console.WriteLine("POST request successful: Created a ToDoItem.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"POST request failed with status code: {response.StatusCode}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception occured: {e.Message}");
+        }
+
+    }
+
     public async Task<List<ToDoItemView>> ReadItemsAsync()
     {
         var ToDoItemsView = new List<ToDoItemView>();
@@ -18,5 +42,84 @@ public class ToDoItemsClient(HttpClient httpClient) : IToDoItemsClient
         }).ToList();
 
         return ToDoItemsView;
+    }
+
+    public async Task<ToDoItemView?> ReadByIdAsync(int itemId)
+    {
+        try
+        {
+            var requestedItemDto = await httpClient.GetFromJsonAsync<ToDoItemGetResponseDto>($"api/ToDoItems/{itemId}");
+
+            if (requestedItemDto is null)
+            {
+                Console.WriteLine($"GET request failed: Item with {itemId} id not found.");
+                throw new ArgumentException($"Given id {itemId} does not exist.");
+            }
+            else
+            {
+                var requestedItemView = new ToDoItemView
+                {
+                    ToDoItemId = requestedItemDto.ToDoItemId,
+                    Name = requestedItemDto.Name,
+                    Description = requestedItemDto.Description,
+                    IsCompleted = requestedItemDto.IsCompleted
+                };
+
+                return requestedItemView;
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception occured: {e.Message}");
+            return null;
+        }
+
+    }
+
+    public async Task UpdateByIdAsync(ToDoItemView toDoItemView)
+    {
+        var toDoItemUpdateDto = new ToDoItemUpdateRequestDto(toDoItemView.Name, toDoItemView.Description, toDoItemView.IsCompleted);
+
+        try
+        {
+            var response = await httpClient.PutAsJsonAsync($"api/ToDoItems/{toDoItemView.ToDoItemId}", toDoItemUpdateDto);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                Console.WriteLine($"PUT request successful: Updated ToDoItem with id {toDoItemView.ToDoItemId}.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"PUT request failed: {response.StatusCode}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception occured: {e.Message}");
+        }
+    }
+
+    public async Task DeleteByIdAsync(ToDoItemView toDoItemView)
+    {
+        try
+        {
+            var response = await httpClient.DeleteAsync($"api/ToDoItems/{toDoItemView.ToDoItemId}");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                Console.WriteLine($"DELETE request successful: Deleted ToDoItem with id {toDoItemView.ToDoItemId}.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"DELETE request failed with status code: {response.StatusCode}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception occured: {e.Message}");
+        }
     }
 }
