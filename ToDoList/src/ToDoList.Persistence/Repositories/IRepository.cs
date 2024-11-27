82341adbad40054765b7655f6a6cd0232391280d
Kristina M <kris.martiskova@gmail.com>
2024-11-27 10:40:52 +0100

    Deleted unused IRepository

--- ToDoList/src/ToDoList.Persistence/Repositories/IRepository.cs
+++ ToDoList/src/ToDoList.Persistence/Repositories/IRepository.cs
@@ -1,14 +1,5 @@
 namespace ToDoList.Persistence.Repositories;
 
-public interface IRepository<T> where T : class
-{
-    public void Create(T item);
-    public T? ReadById(int toDoItemId);
-    public IEnumerable<T> Read();
-    public bool UpdateById(T updatedItem);
-    public bool DeleteById(int toDoItemId);
-}
-
 public interface IRepositoryAsync<T> where T : class
 {
     public Task CreateAsync(T item);
